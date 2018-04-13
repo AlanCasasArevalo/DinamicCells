c13a3aef2d3a807a45f75c21f0222bce3b608274
Alan Casas <alancasasarevalo@gmail.com>
2018-04-13 11:09:32 +0200

    first commit

--- DinamicCells/ViewController.swift
+++ DinamicCells/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DynamicTableViewController: UITableViewController {
+    
+    var dataArray:[String] = ["Alan, electronic technician, happily married, future developer",
+                              "Bibian",
+                              "Theressa",
+                              "Mani",
+                              "Julius",
+                              "Bob",
+                              "My nam is Julian, I born in 1980 and I will be in Italy next years by job"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.estimatedRowHeight = 44
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return dataArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = dataArray[indexPath.row]
+        
+        cell.textLabel?.numberOfLines = 0
+        
+        return cell
+    }
+    
 }
-
---
+++ README.md
@@ -0,0 +1,12 @@
+# GoToShopping
+
+## Introduction
+
+> iOS Dinamic cells
+
+## Preview
+![](https://github.com/AlanCasasArevalo/CustomCellWithXIB/blob/master/Picture.png)
+
+## This simple app is an example
+
+This app is simple app to show how to do dinamic cell depending high of content this cells will be higher or smaller. 
